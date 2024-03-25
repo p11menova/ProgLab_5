01908b04cdc26fae932b8e85ecf234245c8df57a
p11menova <p11menova@yandex.ru>
2024-03-25 17:33:17 +0300

    .idea to .gitignore

---
+++ target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example"},{"l":"org.example.commands"},{"l":"org.example.exceptions"},{"l":"org.example.exceptions.wrongRangeExceptions"},{"l":"org.example.models"},{"l":"org.example.utility"},{"l":"org.example.utility.ModelsAskers"}];updateSearchResults();
